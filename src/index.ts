0e061bea01125f99c6ff98bce0ec941fd4733522
RakshitGumber <gumberrakshit@gmail.com>
2026-01-13 15:36:07 +0530

    added static application

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import { Elysia } from "elysia";
+import { staticPlugin } from "@elysiajs/static";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    await staticPlugin({
+      prefix: "/",
+    })
+  )
+  .listen(3000);
+
+console.log(`Listening on ${app.server!.url}`);
